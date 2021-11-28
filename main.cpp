eba6163acdaa5ce3679c59a8ece17e8734a3ade7
Test <Test@test.com>
2021-11-27 22:28:46 -0500

    Init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Tyler Coleman
+ */
+
+/*
+* Modify the “mini calculator” from exercise 5 to accept (just) single-digit numbers written as either digits or spelled out.
+* 
+* Write a program that performs as a very simple calculator. Your calculator should be able to handle the four basic math
+* operations — add, subtract, multiply, and divide — on two input values. Your program should prompt the user to enter
+* three arguments: two double values and a character to represent an operation. If the entry arguments are 35.6, 24.1, and
+* '+', the program output should be The sum of 35.6 and 24.1 is 59.7. In Chapter 6 we look at a much more
+* sophisticated simple calculator.
+*/
+
